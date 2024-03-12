9247326436f186d3e8fb17f299955c13468f13a4
@olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-03-12 13:11:26 +0100

    hi

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React, {} from 'react';
+
+
+    const Navigation =() =>{
+        return(
+            <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+            <p>Signup</p>
+          </nav>  
+        );
+    }
+
+export default Navigation ;
